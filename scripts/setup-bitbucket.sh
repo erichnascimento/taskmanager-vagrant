c4d6b613e71198e9d4f48cbb49461e0eb830fb74
Erich Nascimento <erichnascimento@gmail.com>
2015-09-24 17:06:24 -0300

    Add bitbucket setup script

---
+++ scripts/setup-bitbucket.sh
@@ -0,0 +1,32 @@
+#! /usr/bin/env bash
+
+set -e
+
+BITBUCKET_GIT_HOST="bitbucket.org"
+KNOWN_HOSTS_FILE="/root/.ssh/known_hosts"
+SSH_CONFIG_FILE="/root/.ssh/config"
+SSH_KEY="/vagrant/ssh/taskmanager-vagrant.rsa"
+SSH_CONFIG="/vagrant/ssh/config"
+
+
+
+setup_bitbucket() {
+
+  #TODO: Refatory this
+  if [[ -f "$KNOWN_HOSTS_FILE" ]]; then
+    "Bitbucket is aready configured! Skipping..." && exit
+  fi
+
+  touch "$KNOWN_HOSTS_FILE"
+
+  # Remove old entry
+  ssh-keygen -R "$BITBUCKET_GIT_HOST"
+
+  # Add new entry
+  # TODO: read fom file
+  ssh-keyscan -H "$BITBUCKET_GIT_HOST" >> ~/.ssh/known_hosts
+  touch "$SSH_CONFIG_FILE"
+  cat "$SSH_CONFIG" >> $SSH_CONFIG_FILE
+}
+
+setup_bitbucket
---
+++ ssh/README.md
@@ -0,0 +1,3 @@
+# SSH configurations
+
+Add here ssh private key
