5044cfedaf79b3c1669e5915fa66866f76aef0d1
Erich Nascimento <erichnascimento@gmail.com>
2015-09-23 23:12:21 -0300

    Add feedback after install mongoDB

--- scripts/install-mongodb.sh
+++ scripts/install-mongodb.sh
@@ -8,23 +8,25 @@ install_mongo() {
   echo "installing MongoDB"
 
   # Import the public key used by the package management system
-  apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+  apt-key -q adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
 
   # Create a list file for MongoDB
   echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" \
     | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
 
   # Reload local package database
-  apt-get update
+  apt-get -q update
 
   # Install the MongoDB packages
-  apt-get install -y mongodb-org
+  apt-get -q install -y mongodb-org
 
   # Start mongodb
   service mongod start
 
   # Clean up
-  apt-get autoremove -y
+  apt-get -q autoremove -y
+
+  echo "MongoDB installed successfully!!!"
 }
 
 check_mongo_installation || install_mongo
