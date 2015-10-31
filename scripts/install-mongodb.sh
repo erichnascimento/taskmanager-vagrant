a88d5a6aa673a0fa02166f7aec23ee19e2a5cc67
Erich Nascimento <erichnascimento@gmail.com>
2015-10-30 22:58:13 -0200

    Remove debug log

--- scripts/install-mongodb.sh
+++ scripts/install-mongodb.sh
@@ -1,5 +1,7 @@
 #! /usr/bin/env bash
 
+set -e
+
 check_mongo_installation()  {
   service mongod status &> /dev/null && echo "Mongo is aready installed! Skipping..."
 }
@@ -8,17 +10,17 @@ install_mongo() {
   echo "installing MongoDB"
 
   # Import the public key used by the package management system
-  apt-key -q adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10
+  apt-key -q adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 7F0CEB10 &> /dev/null
 
   # Create a list file for MongoDB
   echo "deb http://repo.mongodb.org/apt/ubuntu trusty/mongodb-org/3.0 multiverse" \
-    | tee /etc/apt/sources.list.d/mongodb-org-3.0.list
+    | tee /etc/apt/sources.list.d/mongodb-org-3.0.list > /dev/null
 
   # Reload local package database
-  apt-get -q update
+  apt-get -q update > /dev/null
 
   # Install the MongoDB packages
-  apt-get -q install -y mongodb-org
+  apt-get -q install -y --force-yes mongodb-org > /dev/null
 
   # Start mongodb
   service mongod start
